25f4d08e22a3153b9d0062635a2ad04d7cada018
harshal patel <patelharsh0022gmail.com>
2022-09-24 15:43:59 +0530

    initial commit

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte a = 3;
+        char b = 's';
+        long c = 43;
+        int d = 4;
+        double e = 43.54;
+        short f = 2;
+        boolean g = true;
+        boolean h = false;
+        System.out.println("The first data type ie. byte prints:"+a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(h);
+        System.out.println(g);
+        }
+}
