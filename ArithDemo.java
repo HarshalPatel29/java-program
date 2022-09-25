790fd0d3420db24a9b6185a8c86ed9f8bf62348f
harshal patel <patelharsh0022gmail.com>
2022-09-25 14:25:49 +0530

    intial commit

---
+++ ArithDemo.java
@@ -0,0 +1,15 @@
+public class ArithDemo 
+{
+    public static void main(String[] args) 
+    {
+        
+        String a = "20 inches";
+        String b = "20 inches";
+        if(a.equals(b)==true)
+        System.out.println("Both are EQUAL");
+        else
+        System.out.println("Both are not EQUAL");
+
+    }
+    
+}
