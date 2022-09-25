83951763976ea644a0d560ad211993f5c58ce0cb
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-09-25 14:32:49 +0530

    Add files via upload

---
+++ CaseDemo.java
@@ -0,0 +1,14 @@
+public class CaseDemo 
+{
+    public static void main(String[] args)
+    {
+        String str = new String("Harsh is Indian");
+        System.out.println("\n The original string is:"+ str);
+        String str_upper = str.toUpperCase();
+        System.out.println("\n The Upper case string is:"+str_upper);
+        String str_lower = str.toLowerCase();
+        System.out.println("\n The Lower case string is:"+str_lower);
+    }
+
+}
+
