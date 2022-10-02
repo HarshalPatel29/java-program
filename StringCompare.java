a4f15706f6ac777e738f0f432f0563c84d9917a2
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-10-02 10:44:43 +0530

    Add files via upload

---
+++ StringCompare.java
@@ -0,0 +1,12 @@
+public class StringCompare 
+{
+    public static void main(String[] args) 
+    {
+        String str1 = new String("INDIAN");
+        String str2 = new String("INDIAN");
+        if(str1.equals(str2)==true)
+        System.out.println("\n The two strings are equal");
+        else
+        System.out.println("\n The two strings are not equal");
+    }
+}
