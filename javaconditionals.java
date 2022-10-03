e51d46bfd4a285c11b864396bbf7cbde9d01e45b
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-10-03 07:42:44 +0530

    Add files via upload

---
+++ javaconditionals.java
@@ -0,0 +1,12 @@
+public class javaconditionals {
+    public static void main(String[] args) {
+        int age = 23;
+        if(age>18){
+            System.out.println("you can vote");
+        }
+        else{
+            System.out.println("you cannot vote");
+        }        
+    }
+    
+}
