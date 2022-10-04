35bb270d1420381152283685ae0f10c59b8e83ac
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-10-04 18:16:25 +0530

    Add files via upload

---
+++ Enum.java
@@ -0,0 +1,15 @@
+enum MyNumbers 
+{
+One ,Two , Three, Four, Five, Six
+
+}
+public class Enum
+{
+    public static void main(String[] args) {
+        MyNumbers LuckyNumber;
+
+        LuckyNumber = MyNumbers.One;
+        System.out.println("My lucky number is:s "+ LuckyNumber);
+    }
+}
+ 
