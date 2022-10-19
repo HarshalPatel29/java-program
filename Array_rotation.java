d8df76ab03294f936e7e9a41323c775482a45ebb
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-10-19 16:51:40 +0530

    Add files via upload

---
+++ Array_rotation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Array_rotation {
+
+  public static void main (String[] args) throws java.lang.Exception
+  {
+    Scanner sr=new Scanner(System.in);
+    int n=sr.nextInt();
+    int k=sr.nextInt();
+    int q=sr.nextInt();
+    int[] arr=new int [n];
+    for(int i=0;i<n;i++)
+    {
+      arr[i]=sr.nextInt();
+    }
+    int [] rot=new int[n];
+      for(int i=0;i<n;i++)
+      {
+        rot[(i+k)%n]=arr[i];
+      }
+      for(int i=0;i<q;i++)
+      {
+        int index=sr.nextInt();
+        System.out.println(rot[index]);
+      }
+    }
+}
