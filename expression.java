50541fe17aeafdbdd8bb1bafa1d6423ff52cac93
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-10-22 22:09:52 +0530

    Add files via upload

---
+++ expression.java
@@ -0,0 +1,9 @@
+public class expression {
+
+        public static void main(String[] args) {
+            float a = 8 / 5 * 6 / 2;
+            System.out.println("The output of the f0llowing expression is: " + a);
+        }
+    }
+
+
