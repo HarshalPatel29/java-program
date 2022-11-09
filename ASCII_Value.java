f3ddee3646ac2f6062c25cd0665dc24aed960fce
harshal chunilal patel <110116723+HarshalPatel29@users.noreply.github.com>
2022-11-09 19:15:45 +0530

    Add files via upload

---
+++ ASCII_Value.java
@@ -0,0 +1,13 @@
+public class ASCII_Value {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        char ch2 = 'D';
+
+        int Ascii1 = ch1;
+        int Ascii2 = ch2;
+
+        System.out.println("The ASCII value of " + ch1 + " is: " + Ascii1);
+        System.out.println("The ASCII value of " + ch2 + " is: " + Ascii2);
+    }
+}
+
